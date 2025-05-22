066444449efe59b706396ab57f540ef1770dc0f2
aldanadelviso <168225894+aldanadelviso@users.noreply.github.com>
2025-05-22 03:30:11 -0300

    Cambios para agregar mobile y ajustes desktop

--- Web/script.js
+++ Web/script.js
@@ -1,9 +1,33 @@
 document.getElementById("botonFormulario").addEventListener("click", function () {
-  validarFormulario("formulario","nombre","mail","mensajeError","mensajeGracias","popupGracias");
-});
+  event.preventDefault();
+  var nombre = document.getElementById("nombre").value;
+  var mail = document.getElementById("mail").value;
+  var mensajeError = document.getElementById("mensajeError");
 
-document.getElementById("botonFormularioFinal").addEventListener("click", function () {
-  validarFormulario("formularioFinal","nombreFinal","mailFinal","mensajeErrorFinal","mensajeGracias","popupGracias");
+  // Validación básica
+  if (nombre === "" || mail === "") {
+    mensajeError.textContent = "Por favor completá todos los campos.";
+    return;
+  }
+
+  // Validar formato de email simple
+  var emailValido = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(mail);
+  if (!emailValido) {
+    mensajeError.textContent = "Ingresá un email válido.";
+    return;
+  }
+
+  mensajeError.textContent = "";
+
+  var mensaje = document.getElementById("mensajeGracias");
+  var emailPopUp = document.getElementById("emailPopUp");
+  var popup = document.getElementById("popupGracias");
+  mensaje.textContent = `¡GRACIAS ${nombre.toUpperCase()}!`;
+  emailPopUp.textContent= `${mail}`;
+
+  popup.style.display = 'block';
+  var form = document.getElementById("formulario");
+  form.reset();
 });
 
 
@@ -11,33 +35,3 @@ document.getElementById("boton-ok").addEventListener("click", function () {
     var popup = document.getElementById('popupGracias');
     popup.style.display = 'none';
 });
-
-
-function validarFormulario(idFormulario, idNombre, idMail, idMensajeError, idMensajeGracias, idPopup){
-    event.preventDefault();
-    var nombre = document.getElementById(idNombre).value;
-    var mail = document.getElementById(idMail).value;
-    var mensajeError = document.getElementById(idMensajeError);
-
-    // Validación básica
-    if (nombre === "" || mail === "") {
-      mensajeError.textContent = "Por favor completá todos los campos.";
-      return;
-    }
-
-    // Validar formato de email simple
-    var emailValido = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(mail);
-    if (!emailValido) {
-      mensajeError.textContent = "Ingresá un email válido.";
-      return;
-    }
-
-    mensajeError.textContent = "";
-
-    var mensaje = document.getElementById(idMensajeGracias);
-    var popup = document.getElementById(idPopup);
-    mensaje.textContent = `¡GRACIAS ${nombre.toUpperCase()}!`;
-    popup.style.display = 'block';
-    var form = document.getElementById(idFormulario);
-    form.reset();
-}
